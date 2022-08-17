9c67c351235a8f47ededf59a30ba7236cd40bf0b
unknown <quiatus@gmail.com>
2022-08-17 16:06:43 +0200

    version2

--- app.js
+++ app.js
@@ -2,18 +2,29 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
+var items = [];
+
 app.use(bodyParser.urlencoded({extended: true}));
+app.set('view engine', 'ejs');
 
 app.get("/", function(req,res){
-    var today = new Date();
-
-    if (today.getDay() === 6 || today.getDay() === 0) {
-        res.send("It's weekend");
-    } else {
-        // use res.write to send multiple lines, then finish with res.send();
-        res.sendFile(__dirname + '/index.html');
+    let today = new Date();
+    let options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
     }
+    //let currentDay = today.getDay(); 
+    let day = today.toLocaleDateString('en-US', options);
+
+    res.render('list',{kindOfDay: day, newListItem: items});
 });
+
+app.post('/', function(req,res){
+    var item = req.body.addItem;
+    items.push(item);
+    res.redirect('/');
+})
 
 app.listen(3000, function(){
     console.log('Server started on port 3000');
